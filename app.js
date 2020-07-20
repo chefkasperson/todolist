f851b37228f94e18eba1c7f30285f7010642c7d3
chefkasperson <chefkasperson@gmail.com>
2020-07-20 14:50:29 -0700

    added ejs template

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.send('hello')
+    let today = new Date()
+    let currentDay = today.getDay()
+    let day = ''
+
+    if (currentDay === 0) {
+        day = 'Sunday'
+    } else if (currentDay === 1) {
+        day = 'Monday'
+    } else if (currentDay === 2) {
+        day = 'Tuesday'
+    } else if (currentDay === 3) {
+        day = 'Wednesday'
+    } else if (currentDay === 4) {
+        day = 'Thursday' 
+    } else if (currentDay === 5) {
+        day = 'Friday'
+    } else if (currentDay === 6) {
+        day === 'Saturday'
+    }
+    res.render('list', {kindOfDay: day})
 })
 
 app.listen(3000, function() {
