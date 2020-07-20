0c3e0662dc7f4415f85f076cd654e325badc7ada
chefkasperson <chefkasperson@gmail.com>
2020-07-20 15:52:30 -0700

    added ability to add items to list

--- app.js
+++ app.js
@@ -3,56 +3,29 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+
+let items = []
 
 app.get('/', function(req, res) {
     let today = new Date()
-    let currentDay = today.getDay()
-    let day = ''
-
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            break
-        case 1:
-            day = 'Monday'
-            break
-        case 2:
-            day = 'Tuesday'
-            break
-        case 3:
-            day = 'Wednesday'
-            break
-        case 4:
-            day = 'Thurday'
-            break
-        case 5:
-            day = 'Friday'
-            break
-        case 6:
-            day = 'Saturday'
-            break
-        default:
-            console.log('You have left earth. Your current day is ' + currentDay)
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     }
-    // if (currentDay === 0) {
-    //     day = 'Sunday'
-    // } else if (currentDay === 1) {
-    //     day = 'Monday'
-    // } else if (currentDay === 2) {
-    //     day = 'Tuesday'
-    // } else if (currentDay === 3) {
-    //     day = 'Wednesday'
-    // } else if (currentDay === 4) {
-    //     day = 'Thursday' 
-    // } else if (currentDay === 5) {
-    //     day = 'Friday'
-    // } else if (currentDay === 6) {
-    //     day === 'Saturday'
-    // }
-    res.render('list', {kindOfDay: day})
+    let day = today.toLocaleDateString('en-US', options)
+
+   
+    res.render('list', {kindOfDay: day, newListItems: items})
 })
 
+app.post('/', function(req, res) {
+    let item = req.body.newItem
+    items.push(item)
+    res.redirect('/')
+})
 app.listen(3000, function() {
     console.log('Server started on port 3000')
 })
